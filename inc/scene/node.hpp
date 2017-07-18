9f638ab6f143d942e0e66eccf34f990f5a561420
ibrow19 <imb3@st-andrews.ac.uk>
2017-07-18 22:51:43 +0100

    Basic scene node - needs testing

---
+++ inc/scene/node.hpp
@@ -0,0 +1,77 @@
+#ifndef SNAKE_NODE_HEADER
+#define SNAKE_NODE_HEADER
+
+#include <vector>
+#include <memory>
+#include "texture.hpp"
+#include "maths/transform.hpp"
+
+namespace snk {
+
+class Node {
+public:
+
+    Node(Texture* texture);
+
+    void addChild(Node& child);
+
+    //void handleCommand(const Command& command);
+    void render();
+
+    // Note: using dirty flag with local transform data makes some operations impossible
+    //       e.g rotating around a point that is not the origin (translate to point, rotate,
+    //       translate back ~ would just appear like a rotation around the origin).
+
+    // Operations corresponding to the various possible transformations.
+
+    // Origin
+    const Point2f& getOrigin() const;
+    void setOrigin(Point2f origin);
+    void setOrigin(float x, float y);
+
+    // Scale
+    const Vector2f& getScale() const;
+    void setScale(float factor);
+    void setScale(const Vector2f& factor);
+    void setScale(float x, float y);
+    void scale(float factor);
+    void scale(const Vector2f& factor);
+    void scale(float x, float y);
+
+    // Rotate.
+    float getRotation() const;
+    void setRotation(float angle);
+    void rotate(float angle);
+
+    // Translation.
+    const Vector2f& getTranslation() const;
+    void setTranslation(const Vector2f& v);
+    void setTranslation(float x, float y);
+    void translate(const Vector2f& v);
+    void translate(float x, float y);
+
+
+private:
+
+    void render(Transform& world, bool dirty);
+
+private:
+
+    Node* mParent;
+    //ComponentManager& mCManager;
+
+    bool mDirty;
+    Transform mLocal;
+    Transform mWorld;
+
+    Transform::TData mLocalData;
+
+    Texture* mTexture;
+
+    std::vector<Node*> mChildren;
+
+};
+
+} // namespace snk
+
+#endif
---
+++ src/scene/node.cpp
@@ -0,0 +1,197 @@
+#include <cassert>
+#include "scene/node.hpp"
+
+namespace snk {
+
+Node::Node(Texture* texture)
+: mParent(nullptr),
+  mDirty(true),  // Must be true initially to initialise world transform
+  mLocal(),
+  mWorld(),
+  mLocalData({{0.f, 0.f}, 0.f, {0.f, 0.f}, {0.f, 0.f}}),
+  mTexture(texture) {}
+
+
+void Node::addChild(Node& child) {
+
+    child.mParent = this;
+    mChildren.push_back(&child);
+
+}
+
+
+//void handleCommand(const Command& command);
+void Node::render() {
+
+    Transform t;
+    render(t, false);
+
+}
+
+
+// Origin
+const Point2f& Node::getOrigin() const {
+
+    return mLocalData.origin;
+
+}
+
+
+void Node::setOrigin(Point2f origin) {
+
+    setOrigin(origin.x, origin.y);
+
+}
+
+
+void Node::setOrigin(float x, float y) {
+
+    mLocalData.origin.x = x;
+    mLocalData.origin.y = y;
+    mDirty = true;
+
+}
+
+// Scale
+const Vector2f& Node::getScale() const {
+
+    return mLocalData.scale;
+
+}
+
+
+void Node::setScale(float factor) {
+
+    setScale(factor, factor);
+
+}
+
+
+void Node::setScale(const Vector2f& factor) {
+
+    setScale(factor.x, factor.y);
+
+}
+
+
+void Node::setScale(float x, float y) {
+
+    mLocalData.scale.x = x;
+    mLocalData.scale.y = y;
+    mDirty = true;
+
+}
+
+
+void Node::scale(float factor) {
+
+    setScale(factor * mLocalData.scale.x, factor * mLocalData.scale.y);
+
+}
+
+
+void Node::scale(const Vector2f& factor) {
+
+    setScale(factor.x * mLocalData.scale.x, factor.y * mLocalData.scale.y);
+
+}
+
+
+void Node::scale(float x, float y) {
+
+    setScale(x * mLocalData.scale.x, y * mLocalData.scale.y);
+
+}
+
+
+// Rotate.
+float Node::getRotation() const {
+
+    return mLocalData.angle;
+
+}
+
+
+void Node::setRotation(float angle) {
+
+    mLocalData.angle = angle;
+    mDirty = true;
+
+}
+
+
+void Node::rotate(float angle) {
+
+    setRotation(mLocalData.angle + angle);
+
+}
+
+
+// Translation.
+const Vector2f& Node::getTranslation() const {
+
+    return mLocalData.translation;
+
+}
+
+
+void Node::setTranslation(const Vector2f& v) {
+
+    setTranslation(v.x, v.y);
+
+}
+
+
+void Node::setTranslation(float x, float y) {
+
+    mLocalData.translation.x = x;
+    mLocalData.translation.y = y;
+    mDirty = true;
+
+}
+
+
+void Node::translate(const Vector2f& v) {
+
+    setTranslation(v.x + mLocalData.translation.x, v.y + mLocalData.translation.y);
+
+}
+
+
+void Node::translate(float x, float y) {
+
+    setTranslation(x + mLocalData.translation.x, y + mLocalData.translation.y);
+
+}
+
+
+void Node::render(Transform& world, bool dirty) {
+
+    if (mDirty) {
+
+        mLocal = Transform(mLocalData);
+
+    }
+    dirty = dirty || mDirty;
+    mDirty = false;
+    if (dirty) {
+
+        // TODO: this might need to be the other way around.
+        mWorld = mLocal * world; 
+
+    }
+    if (mTexture != nullptr) {
+
+        mTexture->render(mWorld);
+
+    }
+    for (Node* child : mChildren) {
+
+        assert(child != nullptr);
+        child->render(mWorld, dirty);
+
+    }
+
+}
+
+} // namespace snk
